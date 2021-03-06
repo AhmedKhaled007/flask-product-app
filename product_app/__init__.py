37995de911249cdd5346ee5d549c5986f2b04297
Ahmed Khaled Habiba <ahmed.habiba4720@gmail.com>
2021-03-07 01:46:49 +0200

    .

--- product_app/__init__.py
+++ product_app/__init__.py
@@ -11,6 +11,7 @@ def create_app(config_name='development'):
     # app.config.from_pyfile('config.py')
     db.init_app(app)
     migrate = Migrate(app, db)
+    db.create_all()
 
     app.register_blueprint(main)
     return app
